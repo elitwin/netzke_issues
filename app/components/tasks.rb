96e1dc04a0521b22759cd4341dbbeca92c08fbb7
Eric Litwin <eric.litwin@pnmac.com>
2015-02-18 08:33:03 -0800

    Show timestamps for help with debugging date and datetime issues

--- app/components/tasks.rb
+++ app/components/tasks.rb
@@ -8,6 +8,8 @@ class Tasks < Netzke::Basepack::Grid
                  {name: :notes, flex: 1},
                  :priority,
                  {name: :due, header: "Due on"},
+                 {name: :created_at, read_only: true},
+                 {name: :updated_at, read_only: true},
                 ]
     c.scope = {done: [nil, false]}
   end
